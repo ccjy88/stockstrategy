614a03d20a030f666cf328f52c24100985190cb6
ccjy88 <40417260+ccjy88@users.noreply.github.com>
2020-11-16 16:08:16 +0800

    Update stockstrategy.py

--- stockstrategy.py
+++ stockstrategy.py
@@ -1,9 +1,11 @@
 from skhelper import  SkBrain as Agent
+from skhelper import SkdayReader
 from skhelper import setDaysdir
 
 import numpy as np
 import os
 import re
+import time
 
 episodes = 400
 
@@ -13,12 +15,12 @@ agent = Agent('stocknet', globalagent)
 
 '''板块分析。'''
 class GroupParse(object):
-    def __init__(self,endyyyymm,daysdir,printdetailflag):
+    def __init__(self,endyyyymm,daysdir,printdetailflag,delnegflag = True):
         self.endyyyymm = endyyyymm
         self.daysdir = daysdir
-        self.delnegflag = True
+        self.delnegflag = delnegflag
         self.printdetailflag = printdetailflag
-        self.sumscores = []
+        self.monthscores = []
         self.sumlabels = []
 
     def getyyyymm(self,yyyymm):
@@ -42,6 +44,21 @@ class GroupParse(object):
             yyyy -= 1
         return yyyy,mm
 
+    def doParseWeekuprate(self,skid, removeafterdate, percent):
+        setDaysdir(self.daysdir)
+        y,m = self.getyyyymm(removeafterdate)
+        reader = SkdayReader(skid)
+        weekofyear = reader.day2weekofyear(int(y*10000+m*100+1))
+        weeks = reader.toWeek()
+        weeks = reader.removeWeekDateAfter(weeks, y * 100 + weekofyear)
+        weeks = weeks[-12:, ]
+        weeks = reader.calcNextweekHHV(weeks)
+
+        #最后12周
+        success= weeks[ weeks[:, -1] >= percent ]
+        successrate = len(success) / len(weeks) * 100
+        #print('{}周末上涨5成功率{:.1f}%'.format(skid, successrate))
+        return successrate
 
     def doParseGroup(self):
         setDaysdir(self.daysdir)
@@ -54,34 +71,57 @@ class GroupParse(object):
                 agent.setParams()
                 self.calcSkids(skid, removeafterdate)
 
-        self.sumscores = np.array(self.sumscores)
+        self.monthscores = np.array(self.monthscores)
         self.sumlabels = np.array(self.sumlabels)
 
-        if (self.sumscores.shape[0] ==0 ):
+        if (self.monthscores.shape[0] ==0):
             return -1
 
-        gamma = 0.90
-        rate = [np.power(gamma, n) for n in range(self.sumscores.shape[1] - 2, -1, -1)]
-        #折扣加权后的，按行求和。
-        self.sumv = np.sum(self.sumscores[:,:-1] * rate,axis = 1) + self.sumscores[:, -1]*0.5
+        #sum 5:end
+        self.sumv = np.sum(self.monthscores[:, 5: -1], axis=1) + self.monthscores[:, -1] * 0.5
 
-        #删除sumv小于4.0
+        #删除sumv小于4.1
         if self.delnegflag:
-            delindex = np.where(self.sumv < 4.0)
+            delindex = np.where(self.sumv < 4.1)
             self.sumv = np.delete(self.sumv,delindex,axis=0)
             self.sumlabels = np.delete(self.sumlabels,delindex,axis=0)
-            self.sumscores = np.delete(self.sumscores,delindex,axis=0)
+            self.monthscores = np.delete(self.monthscores, delindex, axis=0)
+
+
+        week5arr = []
+        week10arr = []
+        for skid in self.sumlabels:
+            weekup10rate = self.doParseWeekuprate(skid, removeafterdate, 1.10)
+            weekup5rate = self.doParseWeekuprate(skid, removeafterdate, 1.05)
+            week10arr.append(weekup10rate)
+            week5arr.append(weekup5rate)
+        self.monthscores = np.hstack([self.monthscores, np.array(week10arr)[:, None]])
+        self.monthscores = np.hstack([self.monthscores, np.array(week5arr)[:, None]])
+
+        # del 周上涨5%概率小于25%
+        if delnegflag:
+            delindex = np.where(self.monthscores[:, -1] < 25)
+            self.sumv = np.delete(self.sumv, delindex, axis=0)
+            self.sumlabels = np.delete(self.sumlabels, delindex, axis=0)
+            self.monthscores = np.delete(self.monthscores, delindex, axis=0)
+
 
         sortindex = np.argsort(-self.sumv)
         self.sumv = self.sumv[sortindex]
         self.sumlabels = self.sumlabels[sortindex]
-        self.sumscores = self.sumscores[sortindex]
+        self.monthscores = self.monthscores[sortindex]
+
 
         if self.printdetailflag:
-          for i in range(self.sumlabels.shape[0]):
-                print('{} {:04d} {:.1f} {}'.format(self.sumlabels[i], self.endyyyymm,
-                                                     self.sumv[i],self.sumscores[i]))
-        return len(self.sumscores)
+            print('日期:', self.endyyyymm)
+            for i in range(self.sumlabels.shape[0]):
+                print('{}\t{:.1f}\t{}\t{:.1f}\t{:.1f}'.format(self.sumlabels[i]
+                                                              , self.sumv[i]
+                                                              , self.monthscores[i][:-2]
+                                                              , self.monthscores[i][-2]
+                                                              , self.monthscores[i][-1]))
+
+        return len(self.monthscores)
 
     def calcSkids(self, skid, removeafterdate):
         #print('begin calcSkids {}'.format(len(calcSkids)))
@@ -97,35 +137,23 @@ class GroupParse(object):
         futurevs = agent.predict( self.endyyyymm, removeafterdate)
         if len(futurevs) == 0:
              return
-        self.sumscores.append(futurevs)
+        self.monthscores.append(futurevs)
         self.sumlabels.append(skid)
+
+def calcEndmonth(endmonth,daysdir,delnegflag):
+    printdetail = True
+    print('打印{}个股'.format(daysdir))
+    gp = GroupParse(endmonth, daysdir, printdetail, delnegflag)
+    ct = gp.doParseGroup()
+    print(ct)
 
 
 if __name__ == '__main__':
-    #daysdir = r'D:\PycharmProjects\a3c\汽车类'
-    daysdir = r'.\days'
-    #daysdir = r'D:\PycharmProjects\a3c\半导体'
-    daysdir = r'D:\PycharmProjects\a3c\芯片板块'
+    daysdir = r'D:\adatas\days'
+    #daysdir = r'D:\adatas\沪深'
+    endmonth = 202011
 
-    printdetail = True
-    print('{}打印个股'.format(daysdir))
-    gp = GroupParse(202011, daysdir, printdetail)
-    ct = gp.doParseGroup()
-    print(ct)
-    exit(0)
-
-
-    printdetail = False
-    print('{}板块'.format(daysdir))
-    for mm in np.arange(11, 1):
-       yyyymm = 2019 * 100 + mm
-       gp=GroupParse(yyyymm, daysdir, printdetail)
-       ct = gp.doParseGroup()
-       print(yyyymm,ct)
-
-    for mm in np.arange(1, 11):
-        yyyymm = 2020 * 100 + mm
-        gp=GroupParse(yyyymm, daysdir, printdetail)
-        ct = gp.doParseGroup()
-        print(yyyymm,ct)
-
+    #删除回报差的
+    delnegflag = True
+    #delnegflag = False
+    calcEndmonth(endmonth, daysdir, delnegflag)
