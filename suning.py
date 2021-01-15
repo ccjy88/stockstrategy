bb9da08fe2e8254693b6ac4dbfb4b9822d52e276
wwh <wwh@inca.com.cn>
2021-01-15 09:52:51 +0800

    update suning

--- suning.py
+++ suning.py
@@ -1,4 +1,5 @@
 import requests
+from requests.adapters import HTTPAdapter
 import json
 import configparser
 import os
@@ -69,6 +70,8 @@ class SessionBuilder(object):
 
     def buildSession(self, configer,section='head'):
         session = requests.Session()
+        session.mount('http://', HTTPAdapter(max_retries=3))
+        session.mount('https://', HTTPAdapter(max_retries=3))
         session.cookies =requests.utils.cookiejar_from_dict(cookiedict)
         headers={}
         headers['Accept']=configer.getString(section,'accept')
@@ -80,14 +83,6 @@ class SessionBuilder(object):
         session.headers = headers
         return session
 
-    def buildPayload(self, configer):
-        params = configer.getString('params', 'params')
-        ws = params.split('&')
-        paramdict={}
-        for s in ws:
-            nvs=s.split('=')
-            paramdict[nvs[0].strip()] = nvs[1].strip()
-        return paramdict
 
 
 
@@ -130,11 +125,13 @@ def appointment():
     logger.info(respstr)
     issucess, errorcode, errormsg= parseJsonAppointment(respstr)
     if issucess==False:
-        logger.info('预约失败, errorcode={} {}'.format(errorcode,errormsg))
+        weixinmsg= '预约失败, errorcode={} {}'.format(errorcode,errormsg)
+        logger.info(weixinmsg)
+        sendweixin('预约失败', weixinmsg)
     else:
-        logger.info('issucess={}, errorcode={} {}'.format(issucess,errorcode,errormsg))
-    if issucess:
-        sendweixin('预约成功', errormsg)
+        weixinmsg= '预约成功, errorcode={} {}'.format(errorcode,errormsg)
+        logger.info(weixinmsg)
+        sendweixin('预约成功', weixinmsg)
 
 def js2json(str):
     p1 = str.index("({") + 1
@@ -223,9 +220,13 @@ def nowBuy():
     logger.info(respstr)
     issucess,errorcode,errormsg = parseJsonBuy(respstr)
     if issucess==False:
-        logger.info('购买失败, errorcode={} {}'.format(errorcode,errormsg))
+        weixinmsg='购买失败, errorcode={} {}'.format(errorcode,errormsg)
+        logger.info(weixinmsg)
+        sendweixin('购买失败',weixinmsg)
     else:
-        logger.info('issucess={}, errorcode={} {}'.format(issucess,errorcode,errormsg))
+        weixinmsg='issucess={}, errorcode={} {}'.format(issucess,errorcode,errormsg)
+        logger.info(weixinmsg)
+        sendweixin('购买成功',weixinmsg)
 
     return issucess,errorcode,errormsg
 
@@ -242,7 +243,7 @@ def parseJsonBuy(str):
     for resulterror in resultErrorList:
         for edict in resulterror:
             if 'errorCode' in edict:
-                errorCode = edict['errorCode']
+                errorCode = int(edict['errorCode'])
                 errorMessage=edict['errorMessage']
                 #print('错误代码:',edict['errorCode'])
     return  issuccess,errorCode,errorMessage
@@ -255,14 +256,14 @@ def parseJsonAppointment(str):
     obj = json.loads(str)
     errorCode=obj["code"]
     errorMessage=obj['message']
-    issuccess = "0" == errorCode
+    issuccess = int(errorCode) == 0
     return  issuccess,errorCode,errorMessage
 
 #成功后发送微信
 def sendweixin(title,message):
-    global  configer
     if message is None or len(message)==0:
         message='空消息'
+    configer = Config()
     url = configer.getString("weixin","url")
     session = requests.Session()
     payload={"text": title,"desp": message}
@@ -281,11 +282,11 @@ def scheduleStart(task,hour,minute,second,offsetmillisencd):
     login = Login()
     while True:
         differ = target - time.time() + offsetmillisencd/1000.0
-        if differ>60:
-            logger.info('等待60s')
+        if differ>90:
+            logger.info('等待60s......')
             time.sleep(60)
             while Orderlist().isLoginok() == False:
-                logger.info("查询订单失败，需要重新登录,请用手机扫描确认")
+                logger.info("查询订单失败，需要重新登录,请用手机苏宁APP扫描确认")
                 if login.doLogin(): break
         else:
             logger.info("准备启动任务=" + str(differ))
@@ -312,10 +313,13 @@ class Orderlist(object):
     def isLoginok(self):
         logger.info('查询订单验证是否登录....')
         resp = httpget('queryorder')
+        #logger.info(resp.text)
         xdata = etree.HTML(resp.text)
-        inputs = xdata.xpath('//input[@name="receiverAddr_checkAdd"]')
+        inputs = xdata.xpath(r'//div[@class="my-appoint"]')
         #logger.info(len(inputs))
-        return len(inputs)>0 #如果登录成功，应该有多个input
+        loginok = len(inputs)>0 #如果登录成功，应该有多个input
+        logger.info('已登录，查到了我的预约。')
+        return loginok
 
 def str2dict(cookie):
     ws = cookie.split(';')
@@ -338,7 +342,7 @@ def httpget(section,otherheads={},allow_redirects=True):
     url = configer.getString(section, 'url')
     session = SessionBuilder().buildSession(configer, section)
     session.headers.update(otherheads)
-    resp = session.get(url,allow_redirects=allow_redirects)
+    resp = session.get(url,allow_redirects=allow_redirects,timeout=5)
     responseSetCookie(resp.cookies)
     return resp
 
@@ -347,13 +351,13 @@ def httppost(section,data,otherheaders={}):
     url = configer.getString(section, 'url')
     session = SessionBuilder().buildSession(configer, section)
     session.headers.update(otherheaders)
-    resp = session.post(url,data)
+    resp = session.post(url,data,timeout=5)
     responseSetCookie(resp.cookies)
     return resp
 
 cookiedict={}
 bookActionID=''
-partNumber=''
+partNumber='000000011001203841'
 
 #生成新的预约URL，将URL中的活动ID替为最新的bookActionID
 def buildappointmenturl():
@@ -362,8 +366,11 @@ def buildappointmenturl():
     pcdata = js2json(resp.text)
     salenetprice=pcdata['data']['price']['saleInfo'][0]['netPrice']
     bookActionID = pcdata['data']['price']['saleInfo'][0]['bookActionID']
+    if len(bookActionID)==0:
+        logger.error("没有活动，bookActionID={}".format(bookActionID))
+        raise "没有活动，bookActionID={}".format(bookActionID)
     partNumber = pcdata['data']['price']['saleInfo'][0]['partNumber']
-    logger.info('产品ID={} 价格={} 预订ID={}'.format(partNumber,salenetprice,bookActionID))
+    logger.info('产品ID={} 价格={} 活动预订ID={}'.format(partNumber,salenetprice,bookActionID))
     appointmenturl = r'gotoAppoint_{}_{}_0000000000_P01_1_preBuyCallback.do'.format(bookActionID,partNumber)
     appointmenturl='https://yushou.suning.com/jsonp/appoint/'+appointmenturl+r'?callback=preBuyCallback&openId=&dfpToken=THwrjW176f4469b60DIKV3ad6___w7DDp8K5wrxJw6MZbMOxK27CrcOMBMOFw7lawqLCsMOm&appVersion=&detect=mmds_e66gdU6z666XdtDM666CzXDX666NzTDL666IN.DEj666SNyDX666XN-DM666v8Btj666W8utG666l8qtDs666Ol2tL666AlCtB666YGLEX6664GAEL666pMxE0B666RMyEB6663Xt0B666gXN0B666yBT0X666qBH0tL666jB30X666CLp0j6669LK0B666~LssX666SLMsDX666uLNsN666dLess666XLuss6660Lcsj666KjVstB666ojqsX666njrsL666wj1sX666HjHsL666fjasDX666SjQsL666gj2sB6668j3sX666jjysB6666josDL666UsYsX666YsPsL666vsCsX666ns_sL6663s-s67RFD63s-s7n73t3B2e3KbG0x3bE00Gbx6VrbeJre2eKx6E2e76DtE0sjLBX7eJ37n62E7e3E7nn66nJ62eEnkbJ3Vt6nn6J7nV23767n3n6Enr362er277eenrJ663ETD66-DST66lL7tJ272bxtwBc.pZJLPg1Bv7sED66666sED67tJ63eJ2Vtt7Jr2767nV237r7qA6696L6M666n6N6B666v6A6G666Y6q6G6666D266L6660Dp6X666XDtDj666NDMDL666gDFDX666VDTD6B666fDHDX666aDmDB666vD-DL666_DBtB666BtetDX666dtVtB666StrtL666QtatB666It2tB666DEmtEB666lEItB666gEYtB666VEPtB666qECtL666fEUt6X6661EUtX666~E_tZG66hE_tB666OE_tB666G0_tDX666ts_tB666Bj_tB6668LEEL666iBjEB666fXjEtB666rMNEB666cGdEB666LlAEB6663lcEX666i8cEEX66638cEj666dNcEB666bNcEX666PNcEL6668zcEDX666SzcEG666azcEX666ozcE0666jdcEG666RduEtsfErVnJrJne_._1eb18e1f-03ba-478c-b664-67c0a834f6ce_._&referenceURL=https%3A%2F%2Fproduct.suning.com%2F0000000000%2F11001203841.html&_=1610419603946'
     return appointmenturl
@@ -391,6 +398,17 @@ def buildnowbuyurl():
     newurl +=subreq
     return newurl
 
+#从shop主页找飞天maotai
+def searchFeitianURL():
+    resp = httpget('shop')
+    xdata = etree.HTML(resp.text)
+    controls = xdata.xpath('//*[a="飞天茅台"]/a[1]')
+    if len(controls)>0:
+        href = controls[0].attrib['href']
+        return href
+    return None
+
+
 
 if __name__ == '__main__':
 
@@ -398,15 +416,26 @@ if __name__ == '__main__':
     logger=Logger()
     logger.info('begin....')
 
-    appointmenturl = buildappointmenturl()
+    #commodity = searchFeitianURL()
+    #logger.info('飞天茅台主页:'+commodity)
+
+    #生成查询产品销售信息，包括产品价格、预约活动ID.
+    # 生成url,替换产品ID=partNumber
     c = Config()
+    partNumber='000000011001203841'
+    urltemp = c.getString('pcsale', 'urltemp')
+    url = urltemp.format(partNumber, partNumber)
+    c.setString('pcsale','url', url)
+    c.save()
+    appointmenturl = buildappointmenturl()
     c.setString('appointment','url',appointmenturl)
 
+    #生成活动购买URL
     nowbuyurl = buildnowbuyurl()
     c.setString('nowbuy','url',nowbuyurl)
     c.save()
 
-    #全局的cookie
+    #全局的cookie，从上次登录成功的文件读取。cookie不超时就不需要重新登录。
     cookiedict.update(str2dict(readCookiefile()))
     login = Login()
 
@@ -425,6 +454,8 @@ if __name__ == '__main__':
         sys.exit(-1)
 
     if flag == flagdict['预约']:
+
+
         #预约
         schedulehour = c.getInt('appointmenttime','h')
         schedulemin = c.getInt('appointmenttime','m')
